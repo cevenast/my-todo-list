ca89233e3650aa77ca720e2e404f9e1cbdebb9b1
Cristobal Evensen <cevensen.a@gmail.com>
2023-04-27 23:32:21 -0400

    Changes to deploy

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-///////
 const passport = require('passport')
-const session = require('express-session')
+///////   These help us have logged in users
+const session = require('express-session') // Leaves a cookie in our user's computer that matches the session stored in our DB
 const MongoStore = require('connect-mongo')(session)
-const flash = require('express-flash')
-const logger = require('morgan')
 ///////
-const connectDB = require('./config/db-config') // MongoDB Config
+const flash = require('express-flash') //  Helps with the messages that flash up to notify that something went wrong
+const logger = require('morgan') // Shows us our log for every request.
+///////
+const connectDB = require('./config/db-config') // MongoDB Config, connection to our DB
 const homeRoutes = require('./routes/home')
 const todosRoutes = require('./routes/todos')
 //.env
@@ -24,12 +25,12 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(logger('dev'))
+app.use(logger('dev')) // Setting up morgan to run and log everything
 
 // Sessions
-app.use(
+app.use( // Letting our upp use the session
     session({
-      secret: 'keyboard cat',
+      secret: 'keyboard cat', // It can be changed
       resave: false,
       saveUninitialized: false,
       store: new MongoStore({ mongooseConnection: mongoose.connection }),
@@ -37,13 +38,13 @@ app.use(
   )
   
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize()) // Passport's gonna handle our authentication
+app.use(passport.session())    // We're gonna be using session along with passport
 
-app.use(flash())
+app.use(flash())               // Setting the flash alerts that show up when thing go wrong with log in and sign up
 
 
-// Set Routes
+// Routes Set Up
 
 app.use('/',homeRoutes)
 app.use('/todos',todosRoutes)
